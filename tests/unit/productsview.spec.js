6d036982e90b00cd1ff3464708bb75511b4a1aa0
RybochkinaMarina <r.ybochkina@mail.ru>
2022-12-18 02:05:11 +0100

    added test for ProductsView

---
+++ tests/unit/productsview.spec.js
@@ -0,0 +1,32 @@
+import { mount } from '@vue/test-utils'
+import ProductsView from '@/views/ProductsView.vue'
+import ProductCreateForm from '@/components/ProductCreateForm'
+import ProductDeleteForm from '@/components/ProductDeleteForm'
+
+describe('Testing ProductsView.vue', () => {
+  it('should show page title', () => {
+    // when
+    const wrapper = mount(ProductsView)
+
+    // then
+    expect(wrapper.text()).toMatch('Welcome to Products')
+  })
+
+  it('should have product create form component', () => {
+    // when
+    const wrapper = mount(ProductsView)
+
+    // then
+    const createForm = wrapper.findComponent(ProductCreateForm)
+    expect(createForm.exists()).toBeTruthy()
+  })
+
+  it('should have product delete form component', () => {
+    // when
+    const wrapper = mount(ProductsView)
+
+    // then
+    const createForm = wrapper.findComponent(ProductDeleteForm)
+    expect(createForm.exists()).toBeTruthy()
+  })
+})
